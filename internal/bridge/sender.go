58bb02e6eacf5c7df162ce616dfdbb84d9965024
Jacalz <jacob.alzen@gmail.com>
2020-12-14 15:12:06 +0100

    Simplify and optimize file send to use one file open
    The fyne.URIReadCloser does comply with io.ReadSeeker on desktop due to being opened with os.Open(). All we have to do is cast it and we can then avoid opening the file twice.

--- internal/bridge/sender.go
+++ internal/bridge/sender.go
@@ -11,20 +11,9 @@ import (
 )
 
 // NewFileSend takes the chosen file and sends it using wormhole-william.
-func (b *Bridge) NewFileSend(file fyne.URIReadCloser, progress wormhole.SendOption) (string, chan wormhole.SendResult, *os.File, error) {
-	err := file.Close() // Not currently used due to not being an io.ReadSeeker
-	if err != nil {
-		fyne.LogError("Error on closing file", err)
-	}
-
-	f, err := os.Open(file.URI().String()[7:])
-	if err != nil {
-		fyne.LogError("Error on opening file", err)
-		return "", nil, nil, f.Close()
-	}
-
-	code, result, err := b.SendFile(context.Background(), file.URI().Name(), f, progress)
-	return code, result, f, err
+func (b *Bridge) NewFileSend(file fyne.URIReadCloser, progress wormhole.SendOption) (string, chan wormhole.SendResult, fyne.URIReadCloser, error) {
+	code, result, err := b.SendFile(context.Background(), file.URI().Name(), file.(io.ReadSeeker), progress)
+	return code, result, file, err
 }
 
 // NewDirSend takes a listable URI and sends it using wormhole-william.
