529402d63182c573ce15eabcecdb3c4db2b4a6e9
Jacalz <jacob.alzen@gmail.com>
2020-12-25 20:21:25 +0100

    Optimize folder send and remove Windows workaround
    We were using strings.TrimPrefix() to remove the prefix, but that checks if it contains the prefix. We know that all files within a folder will contain it. It also works on Windows becuase URI uses / as separator and filepath.Walk calls with \ (e.g. "C:/home/dir" == "C:\home\dir").

--- internal/transport/sender.go
+++ internal/transport/sender.go
@@ -5,7 +5,6 @@ import (
 	"io"
 	"os"
 	"path/filepath"
-	"runtime"
 
 	"fyne.io/fyne"
 	"github.com/psanford/wormhole-william/wormhole"
@@ -29,10 +28,6 @@ func (c *Client) NewDirSend(dir fyne.ListableURI, progress wormhole.SendOption) 
 			return err
 		} else if info.IsDir() || !info.Mode().IsRegular() {
 			return nil
-		}
-
-		if runtime.GOOS == "windows" {
-			path = filepath.ToSlash(path)
 		}
 
 		files = append(files, wormhole.DirectoryEntry{
