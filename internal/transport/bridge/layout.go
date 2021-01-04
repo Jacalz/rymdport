b6d210131bea13b376a4ca43086aea7779371d01
Jacalz <jacob.alzen@gmail.com>
2021-01-04 22:29:04 +0100

    Update to latest develop for fixes

--- internal/transport/bridge/layout.go
+++ internal/transport/bridge/layout.go
@@ -6,18 +6,19 @@ import (
 	"fyne.io/fyne/widget"
 )
 
-var maxMinSizeHeight int // Keeping all instances of the list layout consistent in height
+var maxMinSizeHeight float32 // Keeping all instances of the list layout consistent in height
 
 type listLayout struct{}
 
 // Layout is called to pack all child objects into a specified size.
 func (g *listLayout) Layout(objects []fyne.CanvasObject, size fyne.Size) {
-	padWidth := (len(objects) - 1) * theme.Padding()
-	cellWidth := (size.Width - padWidth - objects[0].MinSize().Width) / (len(objects) - 1)
+	length := float32(len(objects) - 1)
+	padWidth := length * theme.Padding()
+	cellWidth := (size.Width - padWidth - objects[0].MinSize().Width) / length
 
 	y2 := size.Height
+	oldx, newx := float32(0), y2
 
-	oldx, newx := 0, y2
 	for i, child := range objects {
 		if !child.Visible() {
 			continue
@@ -37,7 +38,7 @@ func (g *listLayout) Layout(objects []fyne.CanvasObject, size fyne.Size) {
 				_, isLabel = cont.Objects[0].(*codeDisplay)
 			}
 
-			if isLabel {
+			if isLabel { // Proper middle alignment for text labels
 				child.Move(fyne.NewPos(oldx, (y2-child.MinSize().Height)/2))
 			} else {
 				child.Move(fyne.NewPos(oldx, 0))
@@ -48,6 +49,7 @@ func (g *listLayout) Layout(objects []fyne.CanvasObject, size fyne.Size) {
 			child.Move(fyne.NewPos(0, 0))
 		}
 
+		// TODO: Better solution for too long text
 		if child.MinSize().Width > cellWidth {
 			oldx += (child.MinSize().Width - cellWidth)
 		}
@@ -59,8 +61,8 @@ func (g *listLayout) Layout(objects []fyne.CanvasObject, size fyne.Size) {
 // MinSize finds the smallest size that satisfies all the child objects.
 // Height will stay consistent between each each instance.
 func (g *listLayout) MinSize(objects []fyne.CanvasObject) fyne.Size {
-	len := len(objects) - 1
-	maxMinSizeWidth := 0
+	length := float32(len(objects) - 1)
+	maxMinSizeWidth := float32(0)
 	for _, child := range objects {
 		if child.Visible() {
 			maxMinSizeWidth = fyne.Max(child.MinSize().Width, maxMinSizeWidth)
@@ -68,5 +70,5 @@ func (g *listLayout) MinSize(objects []fyne.CanvasObject) fyne.Size {
 		}
 	}
 
-	return fyne.NewSize((maxMinSizeWidth+theme.Padding())*len, maxMinSizeHeight+2*theme.Padding())
+	return fyne.NewSize((maxMinSizeWidth+theme.Padding())*length, maxMinSizeHeight+2*theme.Padding())
 }
