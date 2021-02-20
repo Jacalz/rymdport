06c6dbaac6743601925acc5fd400e78ee835a84e
Jacalz <jacob.alzen@gmail.com>
2021-02-20 12:45:55 +0100

    Fix a bug in the layout and optimize it further
    A bug in the layout was causing the text in the progressbar to be off center. This commit also makes sure to optimize the layout further.

--- internal/transport/bridge/layout.go
+++ internal/transport/bridge/layout.go
@@ -16,45 +16,42 @@ func (g *listLayout) Layout(objects []fyne.CanvasObject, size fyne.Size) {
 	padWidth := length * theme.Padding()
 	cellWidth := (size.Width - padWidth - objects[0].MinSize().Width) / length
 
-	y2 := size.Height
-	oldx, newx := float32(0), y2
-
+	start, end := float32(0), size.Height
 	for i, child := range objects {
 		if !child.Visible() {
 			continue
 		}
 
-		if i != 0 {
-			if i == 1 {
-				oldx += newx + theme.Padding()
-			} else {
-				oldx += cellWidth + theme.Padding()
-			}
-
-			newx = oldx + cellWidth
-
-			_, isLabel := child.(*widget.Label)
-			if cont, ok := child.(*fyne.Container); ok {
-				_, isLabel = cont.Objects[0].(*codeDisplay)
-			}
-
-			if isLabel { // Proper middle alignment for text labels
-				child.Move(fyne.NewPos(oldx, (y2-child.MinSize().Height)/2))
-			} else {
-				child.Move(fyne.NewPos(oldx, 0))
-			}
-
-		} else {
-			newx = size.Height + theme.Padding()
+		if i == 0 {
 			child.Move(fyne.NewPos(0, 0))
+			child.Resize(fyne.NewSize(size.Height, size.Height))
+			start = end + theme.Padding()
+			continue
+		}
+
+		if i != 1 {
+			start += cellWidth + theme.Padding()
+		}
+
+		end = start + cellWidth
+
+		_, isLabel := child.(*widget.Label)
+		if cont, ok := child.(*fyne.Container); ok {
+			_, isLabel = cont.Objects[0].(*codeDisplay)
+		}
+
+		if isLabel { // Proper vertical alignment for text labels
+			child.Move(fyne.NewPos(start, (size.Height-child.MinSize().Height)/2))
+		} else {
+			child.Move(fyne.NewPos(start, 0))
 		}
 
 		// TODO: Better solution for too long text
-		if child.MinSize().Width > cellWidth {
-			oldx += (child.Size().Width - cellWidth)
+		if width := child.MinSize().Width; width > cellWidth {
+			start += width - cellWidth
 		}
 
-		child.Resize(fyne.NewSize(newx-oldx, y2))
+		child.Resize(fyne.NewSize(end-start, size.Height))
 	}
 }
 
