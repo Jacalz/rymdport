e58cda9b93576711d7a0c98cb18f8d743ae63d51
Jacalz <jacob.alzen@gmail.com>
2021-03-08 15:25:22 +0100

    Make sure to correctly create sub directories

--- internal/transport/zip/zip.go
+++ internal/transport/zip/zip.go
@@ -14,6 +14,7 @@ import (
 var errorDangerousFilename = errors.New("dangerous filename detected")
 
 // Extract takes a reader and the length and then extracts it to the target.
+// The target should be the path to a folder where the extracted files can be placed.
 func Extract(source io.ReaderAt, length int64, target string) error {
 	reader, err := zip.NewReader(source, length)
 	if err != nil {
@@ -56,13 +57,17 @@ func extractFile(file *zip.File, target string) (err error) {
 	}()
 
 	if file.FileInfo().IsDir() {
-		err = os.MkdirAll(path, 0750)
-		if err != nil {
+		if err := os.MkdirAll(path, 0750); err != nil {
 			fyne.LogError("Could not create the directory", err)
 			return err
 		}
 
 		return nil
+	}
+
+	if err := os.MkdirAll(filepath.Dir(path), 0750); err != nil {
+		fyne.LogError("Could not create the directory", err)
+		return err
 	}
 
 	targetFile, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, file.Mode()) // #nosec - The path has already been cleaned by filepath.Abs()
