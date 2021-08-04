bac5281360cde47582da0f4f528c9f4d56a87f0c
Jacob Alzen <jacob.alzen@gmail.com>
2021-08-04 16:07:58 +0200

    Improve startup time by avoiding re-parsing URL
    Should improve the startup time slightly by defining the *url.URL directly instead of parsing it from the string each time the application starts.

--- internal/ui/about.go
+++ internal/ui/about.go
@@ -12,10 +12,13 @@ import (
 	"github.com/Jacalz/wormhole-gui/internal/assets"
 )
 
-const (
-	version    = "v2.2.2"
-	releaseURL = "https://github.com/Jacalz/wormhole-gui/releases/tag/" + version
-)
+const version = "v2.2.2"
+
+var releaseURL = &url.URL{
+	Scheme: "https",
+	Host:   "github.com",
+	Path:   "/Jacalz/wormhole-gui/releases/tag/" + version,
+}
 
 type about struct {
 	icon        *canvas.Image
@@ -34,7 +37,7 @@ func (a *about) buildUI() *fyne.Container {
 
 	a.nameLabel = newBoldLabel("wormhole-gui")
 	a.spacerLabel = newBoldLabel("-")
-	a.hyperlink = &widget.Hyperlink{Text: version, URL: parseURL(releaseURL), TextStyle: fyne.TextStyle{Bold: true}}
+	a.hyperlink = &widget.Hyperlink{Text: version, URL: releaseURL, TextStyle: fyne.TextStyle{Bold: true}}
 
 	return container.NewVBox(
 		layout.NewSpacer(),
@@ -52,13 +55,4 @@ func (a *about) buildUI() *fyne.Container {
 
 func (a *about) tabItem() *container.TabItem {
 	return &container.TabItem{Text: "About", Icon: theme.InfoIcon(), Content: a.buildUI()}
-}
-
-func parseURL(input string) *url.URL {
-	link, err := url.Parse(input)
-	if err != nil {
-		fyne.LogError("Could not parse URL string", err)
-	}
-
-	return link
 }
