bc8d06e9ae5114a477344ca8130f86f9fd24d680
Jacob Alzen <jacalz@users.noreply.github.com>
2022-06-06 17:54:39 +0200

    Add tests for the util package

---
+++ internal/util/util_test.go
@@ -0,0 +1,38 @@
+package util
+
+import (
+	"testing"
+
+	"fyne.io/fyne/v2"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCodeValidator(t *testing.T) {
+	validate := CodeValidator
+
+	// This is so that the Entry doesn't report invalid when empty.
+	valid := validate("")
+	assert.NoError(t, valid)
+
+	valid = validate("invalid-code")
+	assert.Error(t, valid)
+
+	valid = validate("15")
+	assert.Error(t, valid)
+
+	valid = validate("15-valid")
+	assert.NoError(t, valid)
+
+	valid = validate("15-valid-")
+	assert.Error(t, valid)
+
+	valid = validate("15-valid-code")
+	assert.NoError(t, valid)
+}
+
+func TestWindowSizeToDialog(t *testing.T) {
+	factor := float32(0.8)
+	actual := fyne.NewSize(100, 200)
+	expected := fyne.NewSize(actual.Width*factor, actual.Height*factor)
+	assert.Equal(t, expected, WindowSizeToDialog(actual))
+}
