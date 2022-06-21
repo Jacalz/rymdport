4c6985d390666d275a18cdc149f229787f5ac2cf
Jacob Alzen <jacalz@users.noreply.github.com>
2022-06-21 19:01:36 +0200

    Add keyboard shortcuts for cycling through tabs
    This adds support for cycling through the tabs using Alt + Tab like in browsers.

--- internal/ui/tabs.go
+++ internal/ui/tabs.go
@@ -4,6 +4,7 @@ package ui
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/driver/desktop"
 	"github.com/Jacalz/rymdport/v3/internal/transport"
 )
 
@@ -11,10 +12,23 @@ import (
 func Create(app fyne.App, window fyne.Window) *container.AppTabs {
 	client := transport.NewClient(app)
 
-	return &container.AppTabs{Items: []*container.TabItem{
+	tabs := &container.AppTabs{Items: []*container.TabItem{
 		newSend(app, window, client).tabItem(),
 		newRecv(app, window, client).tabItem(),
 		newSettings(app, window, client).tabItem(),
 		newAbout(app).tabItem(),
 	}}
+
+	// TODO: Tidy this up once we add mobile support.
+	ctrlTab := &desktop.CustomShortcut{KeyName: fyne.KeyTab, Modifier: fyne.KeyModifierControl}
+	window.Canvas().AddShortcut(ctrlTab, func(shortcut fyne.Shortcut) {
+		next := tabs.SelectedIndex() + 1
+		if next >= len(tabs.Items) {
+			next = 0
+		}
+
+		tabs.SelectIndex(next)
+	})
+
+	return tabs
 }
