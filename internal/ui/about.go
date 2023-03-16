338fe53449f1b0a9b798d572d03159a894d2d066
Jacalz <Jacalz@users.noreply.github.com>
2023-03-16 14:19:06 +0100

    Minor preparations for upcoming v3.3.3

--- internal/ui/about.go
+++ internal/ui/about.go
@@ -29,7 +29,7 @@ func (a *about) buildUI() *fyne.Container {
 	const (
 		https   = "https"
 		github  = "github.com"
-		version = "v3.3.2"
+		version = "v3.3.3"
 	)
 
 	repoURL := &url.URL{Scheme: https, Host: github, Path: "/jacalz/rymdport"}
