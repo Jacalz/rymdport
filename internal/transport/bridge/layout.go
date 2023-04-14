caa4d4dac9ac39c0cc6a955619010e6aa9491f4a
Jacalz <jacalz@tutanota.com>
2023-04-14 08:14:40 +0200

    bridge: Cache inner padding in layout for improved speed

--- internal/transport/bridge/layout.go
+++ internal/transport/bridge/layout.go
@@ -11,23 +11,24 @@ var maxMinSizeHeight float32 // Keeping all instances of the list layout consist
 type listLayout struct{}
 
 func (g *listLayout) Layout(objects []fyne.CanvasObject, size fyne.Size) {
+	innerPadding := theme.InnerPadding()
 	doublePadding := 2 * theme.InnerPadding()
 
-	objects[0].Move(fyne.NewPos(0, theme.InnerPadding()))
+	objects[0].Move(fyne.NewPos(0, innerPadding))
 	objects[0].Resize(fyne.NewSize(size.Height-theme.Padding(), size.Height-doublePadding))
 
 	cellSize := (size.Width - size.Height - doublePadding) / (float32(len(objects) - 1))
-	start, end := size.Height, size.Height+cellSize-theme.InnerPadding()
+	start, end := size.Height, size.Height+cellSize-innerPadding
 	for _, child := range objects[1:] {
 		if _, label := child.(*widget.Label); label {
 			child.Move(fyne.NewPos(start, (size.Height-child.MinSize().Height)/2))
 		} else {
-			child.Move(fyne.NewPos(start, theme.InnerPadding()))
+			child.Move(fyne.NewPos(start, innerPadding))
 		}
 
 		child.Resize(fyne.NewSize(end-start, size.Height-doublePadding))
 
-		start = end + theme.InnerPadding()
+		start = end + innerPadding
 		end = start + cellSize
 	}
 }
