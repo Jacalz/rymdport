a2f7af618a046692dc71b45c477fd48709312bb0
Jacalz <Jacalz@users.noreply.github.com>
2023-05-11 21:42:10 +0200

    transport: Fix possible crash when copying text from text receive window
    If the window hadn't been shown before then there would be a crash due to there being no window viewport for the clipboard to attach to. Should fix one of the crashes in the issue linked below.
    
    For #83

--- internal/transport/display.go
+++ internal/transport/display.go
@@ -46,12 +46,10 @@ func (c *Client) showTextReceiveWindow(received []byte) {
 		d.textEntry.SetText("")
 	})
 
-	clipboard := d.window.Clipboard()
 	d.copyButton.OnTapped = func() {
-		clipboard.SetContent(text)
+		d.window.Clipboard().SetContent(text)
 	}
 
-	canvas := d.window.Canvas()
 	d.saveButton.OnTapped = func() {
 		go func() {
 			save := dialog.NewFileSave(func(file fyne.URIWriteCloser, err error) { // TODO: Might want to save this instead of recreating each time
@@ -75,7 +73,7 @@ func (c *Client) showTextReceiveWindow(received []byte) {
 			}, d.window)
 			now := time.Now().Format("2006-01-02T15:04")
 			save.SetFileName("received-" + now + ".txt")
-			save.Resize(util.WindowSizeToDialog(canvas.Size()))
+			save.Resize(util.WindowSizeToDialog(d.window.Canvas().Size()))
 			save.Show()
 		}()
 	}
@@ -83,7 +81,7 @@ func (c *Client) showTextReceiveWindow(received []byte) {
 	d.textEntry.SetText(text)
 	d.window.Show()
 	d.window.RequestFocus()
-	canvas.Focus(d.textEntry)
+	d.window.Canvas().Focus(d.textEntry)
 }
 
 type textSendWindow struct {
