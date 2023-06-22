ff221f5fd7e8e101b3ba17eeb3ee9bc2684a74ff
Jacalz <Jacalz@users.noreply.github.com>
2023-06-22 20:30:58 +0200

    Switch to the send tab automatically on drag and drop

--- internal/ui/send.go
+++ internal/ui/send.go
@@ -20,16 +20,18 @@ type send struct {
 	data *bridge.SendData
 
 	client *transport.Client
-	canvas fyne.Canvas
+	window fyne.Window
 }
 
-func newSendTab(w fyne.Window, c *transport.Client) *container.TabItem {
-	send := &send{client: c, canvas: w.Canvas()}
+func newSend(w fyne.Window, c *transport.Client) *send {
+	return &send{client: c, window: w}
+}
 
+func (s *send) newSendTab() *container.TabItem {
 	return &container.TabItem{
 		Text:    "Send",
 		Icon:    theme.MailSendIcon(),
-		Content: send.buildUI(w),
+		Content: s.buildUI(s.window),
 	}
 }
 
@@ -42,12 +44,11 @@ func (s *send) buildUI(window fyne.Window) *fyne.Container {
 	choiceContent := container.NewGridWithColumns(1, fileChoice, directoryChoice, textChoice, codeChoice)
 	s.contentPicker = dialog.NewCustom("Pick a content type", "Cancel", choiceContent, window)
 
-	s.data = &bridge.SendData{Client: s.client, Window: window, Canvas: s.canvas}
+	s.data = &bridge.SendData{Client: s.client, Window: window, Canvas: s.window.Canvas()}
 	contentToSend := &widget.Button{Text: "Add content to send", Icon: theme.ContentAddIcon(), OnTapped: s.contentPicker.Show}
 
 	s.fileDialog = dialog.NewFileOpen(s.data.OnFileSelect, window)
 	s.directoryDialog = dialog.NewFolderOpen(s.data.OnDirSelect, window)
-	window.SetOnDropped(s.onDropped)
 
 	box := container.NewVBox(&widget.Separator{}, contentToSend, &widget.Separator{})
 	return container.NewBorder(box, nil, nil, nil, bridge.NewSendList(s.data))
@@ -55,13 +56,13 @@ func (s *send) buildUI(window fyne.Window) *fyne.Container {
 
 func (s *send) onFileSend() {
 	s.contentPicker.Hide()
-	s.fileDialog.Resize(util.WindowSizeToDialog(s.canvas.Size()))
+	s.fileDialog.Resize(util.WindowSizeToDialog(s.window.Canvas().Size()))
 	s.fileDialog.Show()
 }
 
 func (s *send) onDirSend() {
 	s.contentPicker.Hide()
-	s.fileDialog.Resize(util.WindowSizeToDialog(s.canvas.Size()))
+	s.fileDialog.Resize(util.WindowSizeToDialog(s.window.Canvas().Size()))
 	s.directoryDialog.Show()
 }
 
@@ -79,23 +80,22 @@ func (s *send) onDropped(_ fyne.Position, uris []fyne.URI) {
 		return
 	}
 
-	// TODO: Switch to send tab if not on it already.
-
-	if len(uris) == 1 {
-		isDir, err := storage.CanList(uris[0])
-		if err != nil {
-			fyne.LogError("Could not check if path is directory", err)
-			return
-		}
-
-		if !isDir {
-			reader, err := storage.Reader(uris[0])
-			s.data.OnFileSelect(reader, err)
-		} else {
-			reader, err := storage.ListerForURI(uris[0])
-			s.data.OnDirSelect(reader, err)
-		}
-	} else {
+	if len(uris) > 1 {
 		s.data.NewSendFromFiles(uris)
+		return
+	}
+
+	isDir, err := storage.CanList(uris[0])
+	if err != nil {
+		fyne.LogError("Could not check if path is directory", err)
+		return
+	}
+
+	if !isDir {
+		reader, err := storage.Reader(uris[0])
+		s.data.OnFileSelect(reader, err)
+	} else {
+		reader, err := storage.ListerForURI(uris[0])
+		s.data.OnDirSelect(reader, err)
 	}
 }
