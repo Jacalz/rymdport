b13a7f1852ca74ed857a5181115586d28acd2714
Jacalz <Jacalz@users.noreply.github.com>
2023-08-14 14:34:27 +0200

    Minor cleanup of text receive window code

--- internal/transport/display.go
+++ internal/transport/display.go
@@ -17,6 +17,11 @@ type textRecvWindow struct {
 	window                 fyne.Window
 }
 
+func (t *textRecvWindow) interceptClose() {
+	t.window.Hide()
+	t.textEntry.SetText("")
+}
+
 func createTextRecvWindow(app fyne.App) *textRecvWindow {
 	display := &textRecvWindow{
 		window:     app.NewWindow("Received Text"),
@@ -24,6 +29,8 @@ func createTextRecvWindow(app fyne.App) *textRecvWindow {
 		copyButton: &widget.Button{Text: "Copy", Icon: theme.ContentCopyIcon()},
 		saveButton: &widget.Button{Text: "Save", Icon: theme.DocumentSaveIcon()},
 	}
+
+	display.window.SetCloseIntercept(display.interceptClose)
 
 	actionContainer := container.NewGridWithColumns(2, display.copyButton, display.saveButton)
 	display.window.SetContent(container.NewBorder(nil, actionContainer, nil, nil, display.textEntry))
@@ -41,41 +48,34 @@ func (c *Client) showTextReceiveWindow(received []byte) {
 	d := c.textRecvWindow
 	text := string(received)
 
-	d.window.SetCloseIntercept(func() {
-		d.window.Hide()
-		d.textEntry.SetText("")
-	})
-
 	d.copyButton.OnTapped = func() {
 		d.window.Clipboard().SetContent(text)
 	}
 
 	d.saveButton.OnTapped = func() {
-		go func() {
-			save := dialog.NewFileSave(func(file fyne.URIWriteCloser, err error) { // TODO: Might want to save this instead of recreating each time
-				if err != nil {
-					fyne.LogError("Error on selecting file to write to", err)
-					dialog.ShowError(err, d.window)
-					return
-				} else if file == nil {
-					return
-				}
+		save := dialog.NewFileSave(func(file fyne.URIWriteCloser, err error) { // TODO: Might want to save this instead of recreating each time
+			if err != nil {
+				fyne.LogError("Error on selecting file to write to", err)
+				dialog.ShowError(err, d.window)
+				return
+			} else if file == nil {
+				return
+			}
 
-				if _, err := file.Write(received); err != nil {
-					fyne.LogError("Error on writing text to the file", err)
-					dialog.ShowError(err, d.window)
-				}
+			if _, err := file.Write(received); err != nil {
+				fyne.LogError("Error on writing text to the file", err)
+				dialog.ShowError(err, d.window)
+			}
 
-				if err := file.Close(); err != nil {
-					fyne.LogError("Error on closing text file", err)
-					dialog.ShowError(err, d.window)
-				}
-			}, d.window)
-			now := time.Now().Format("2006-01-02T15:04")
-			save.SetFileName("received-" + now + ".txt")
-			save.Resize(util.WindowSizeToDialog(d.window.Canvas().Size()))
-			save.Show()
-		}()
+			if err := file.Close(); err != nil {
+				fyne.LogError("Error on closing text file", err)
+				dialog.ShowError(err, d.window)
+			}
+		}, d.window)
+		now := time.Now().Format("2006-01-02T15:04")
+		save.SetFileName("received-" + now + ".txt")
+		save.Resize(util.WindowSizeToDialog(d.window.Canvas().Size()))
+		save.Show()
 	}
 
 	d.textEntry.SetText(text)
