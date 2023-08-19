2033319717f4bfd71ec2a0c8f8c62251c7f9112b
Jacalz <Jacalz@users.noreply.github.com>
2023-08-19 17:55:49 +0200

    Add proper ellipsis truncation to code
    Fixes #96

--- internal/transport/bridge/bridge.go
+++ internal/transport/bridge/bridge.go
@@ -11,7 +11,7 @@ import (
 )
 
 func newCodeDisplay(window fyne.Window) *fyne.Container {
-	codeLabel := &widget.Label{Text: "Waiting for code..."}
+	codeLabel := &widget.Label{Text: "Waiting for code...", Truncation: fyne.TextTruncateEllipsis}
 	copyButton := &widget.Button{Icon: theme.ContentCopyIcon(), Importance: widget.LowImportance}
 
 	copyButton.OnTapped = func() {
@@ -26,5 +26,25 @@ func newCodeDisplay(window fyne.Window) *fyne.Container {
 		copyButton.SetIcon(theme.ContentCopyIcon())
 	}
 
-	return container.NewHBox(codeLabel, copyButton)
+	return container.New(codeLayout{}, codeLabel, copyButton)
+}
+
+type codeLayout struct{}
+
+func (c codeLayout) Layout(objects []fyne.CanvasObject, size fyne.Size) {
+	displacement := size.Width * 0.8
+
+	objects[0].Move(fyne.NewSquareOffsetPos(0))
+	objects[0].Resize(fyne.NewSize(displacement, size.Height))
+
+	objects[1].Move(fyne.NewPos(displacement, 0))
+	objects[1].Resize(fyne.NewSquareSize(size.Height))
+
+}
+
+func (c codeLayout) MinSize(objects []fyne.CanvasObject) fyne.Size {
+	leftMin := objects[0].MinSize()
+	rightMin := objects[1].MinSize()
+
+	return fyne.NewSize(leftMin.Width+leftMin.Width, fyne.Max(leftMin.Height, rightMin.Height))
 }
--- internal/transport/bridge/recv.go
+++ internal/transport/bridge/recv.go
@@ -60,7 +60,7 @@ func (d *RecvData) CreateItem() fyne.CanvasObject {
 	return container.New(&listLayout{},
 		&widget.FileIcon{},
 		&widget.Label{Text: "Waiting for filename...", Truncation: fyne.TextTruncateEllipsis},
-		&widget.Label{Text: "Waiting for code..."},
+		&widget.Label{Text: "Waiting for code...", Truncation: fyne.TextTruncateEllipsis},
 		&widget.ProgressBar{},
 	)
 }
