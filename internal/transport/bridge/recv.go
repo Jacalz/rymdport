4a46c9d63337cab07189ade7d359342f03b039e3
Jacalz <Jacalz@users.noreply.github.com>
2023-11-13 15:35:12 +0100

    Move item removal out to a separate method

--- internal/transport/bridge/recv.go
+++ internal/transport/bridge/recv.go
@@ -98,27 +98,7 @@ func (d *RecvData) OnSelected(i int) {
 	var infoDialog *dialog.CustomDialog
 	removeLabel := &widget.Label{Text: "This item has completed the transfer and can be removed."}
 	removeButton := &widget.Button{Icon: theme.DeleteIcon(), Importance: widget.DangerImportance, Text: "Remove", OnTapped: func() {
-		// Make sure that no updates happen while we modify the slice.
-		d.deleting.Store(true)
-
-		if i < len(d.items)-1 {
-			copy(d.items[i:], d.items[i+1:])
-		}
-
-		d.items[len(d.items)-1] = nil // Allow the GC to reclaim memory.
-		d.items = d.items[:len(d.items)-1]
-
-		// Update the moved items to have the correct index.
-		for j := i; j < len(d.items); j++ {
-			d.items[j].index = j
-		}
-
-		// Refresh the whole list.
-		d.list.Refresh()
-
-		// Allow individual objects to be refreshed again.
-		d.deleting.Store(false)
-
+		d.remove(i)
 		infoDialog.Hide()
 		infoDialog = nil
 	}}
@@ -168,6 +148,29 @@ func (d *RecvData) refresh(index int) {
 	}
 
 	d.list.RefreshItem(index)
+}
+
+func (d *RecvData) remove(index int) {
+	// Make sure that no updates happen while we modify the slice.
+	d.deleting.Store(true)
+
+	if index < len(d.items)-1 {
+		copy(d.items[index:], d.items[index+1:])
+	}
+
+	d.items[len(d.items)-1] = nil // Allow the GC to reclaim memory.
+	d.items = d.items[:len(d.items)-1]
+
+	// Update the moved items to have the correct index.
+	for j := index; j < len(d.items); j++ {
+		d.items[j].index = j
+	}
+
+	// Refresh the whole list.
+	d.list.Refresh()
+
+	// Allow individual objects to be refreshed again.
+	d.deleting.Store(false)
 }
 
 // NewRecvList greates a list of progress bars.
--- internal/transport/bridge/send.go
+++ internal/transport/bridge/send.go
@@ -119,27 +119,7 @@ func (d *SendData) OnSelected(i int) {
 	var infoDialog *dialog.CustomDialog
 	removeLabel := &widget.Label{Text: "This item can be removed.\nThe transfer has completed."}
 	removeButton := &widget.Button{Icon: theme.DeleteIcon(), Importance: widget.DangerImportance, Text: "Remove", OnTapped: func() {
-		// Make sure that no updates happen while we modify the slice.
-		d.deleting.Store(true)
-
-		if i < len(d.items)-1 {
-			copy(d.items[i:], d.items[i+1:])
-		}
-
-		d.items[len(d.items)-1] = nil // Allow the GC to reclaim memory.
-		d.items = d.items[:len(d.items)-1]
-
-		// Update the moved items to have the correct index.
-		for j := i; j < len(d.items); j++ {
-			d.items[j].index = j
-		}
-
-		// Refresh the whole list.
-		d.list.Refresh()
-
-		// Allow individual objects to be refreshed again.
-		d.deleting.Store(false)
-
+		d.remove(i)
 		infoDialog.Hide()
 		infoDialog = nil
 	}}
@@ -355,6 +335,29 @@ func (d *SendData) refresh(index int) {
 	}
 
 	d.list.RefreshItem(index)
+}
+
+func (d *SendData) remove(index int) {
+	// Make sure that no updates happen while we modify the slice.
+	d.deleting.Store(true)
+
+	if index < len(d.items)-1 {
+		copy(d.items[index:], d.items[index+1:])
+	}
+
+	d.items[len(d.items)-1] = nil // Allow the GC to reclaim memory.
+	d.items = d.items[:len(d.items)-1]
+
+	// Update the moved items to have the correct index.
+	for j := index; j < len(d.items); j++ {
+		d.items[j].index = j
+	}
+
+	// Refresh the whole list.
+	d.list.Refresh()
+
+	// Allow individual objects to be refreshed again.
+	d.deleting.Store(false)
 }
 
 // NewSendList greates a list of progress bars.
