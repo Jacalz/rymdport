a90c3ba557593c7632ff147e3d0ce44abf22b94a
Jacob <jacalz@tutanota.com>
2024-01-11 15:16:16 +0100

    Rework textReceiveWindow to not recreate tap handlers

--- internal/transport/display.go
+++ internal/transport/display.go
@@ -15,12 +15,42 @@ type textRecvWindow struct {
 	textEntry              *widget.Entry
 	copyButton, saveButton *widget.Button
 	window                 fyne.Window
-	// TODO: Save received text here and avoid creating new functions each time?
+	received               []byte
+}
+
+func (r *textRecvWindow) copy() {
+	r.window.Clipboard().SetContent(string(r.received))
 }
 
 func (r *textRecvWindow) interceptClose() {
 	r.window.Hide()
 	r.textEntry.SetText("")
+}
+
+func (r *textRecvWindow) save() {
+	save := dialog.NewFileSave(func(file fyne.URIWriteCloser, err error) { // TODO: Might want to save this instead of recreating each time
+		if err != nil {
+			fyne.LogError("Error on selecting file to write to", err)
+			dialog.ShowError(err, r.window)
+			return
+		} else if file == nil {
+			return
+		}
+
+		if _, err := file.Write(r.received); err != nil {
+			fyne.LogError("Error on writing text to the file", err)
+			dialog.ShowError(err, r.window)
+		}
+
+		if err := file.Close(); err != nil {
+			fyne.LogError("Error on closing text file", err)
+			dialog.ShowError(err, r.window)
+		}
+	}, r.window)
+	now := time.Now().Format("2006-01-02T15:04") // TODO: Might want to use AppendFormat and strings.Builder
+	save.SetFileName("received-" + now + ".txt")
+	save.Resize(util.WindowSizeToDialog(r.window.Canvas().Size()))
+	save.Show()
 }
 
 func (c *Client) createTextRecvWindow() {
@@ -30,8 +60,8 @@ func (c *Client) createTextRecvWindow() {
 	c.textRecvWindow = textRecvWindow{
 		window:     window,
 		textEntry:  &widget.Entry{MultiLine: true, Wrapping: fyne.TextWrapWord},
-		copyButton: &widget.Button{Text: "Copy", Icon: theme.ContentCopyIcon()},
-		saveButton: &widget.Button{Text: "Save", Icon: theme.DocumentSaveIcon()},
+		copyButton: &widget.Button{Text: "Copy", Icon: theme.ContentCopyIcon(), OnTapped: c.textRecvWindow.copy},
+		saveButton: &widget.Button{Text: "Save", Icon: theme.DocumentSaveIcon(), OnTapped: c.textRecvWindow.save},
 	}
 
 	actionContainer := container.NewGridWithColumns(2, c.textRecvWindow.copyButton, c.textRecvWindow.saveButton)
@@ -45,41 +75,11 @@ func (c *Client) showTextReceiveWindow(received []byte) {
 		c.createTextRecvWindow()
 	} // else: Might want to request window focus?
 
-	d := c.textRecvWindow
-	text := string(received)
+	c.textRecvWindow.received = received
+	c.textRecvWindow.textEntry.SetText(string(received))
 
-	d.copyButton.OnTapped = func() {
-		d.window.Clipboard().SetContent(text)
-	}
-
-	d.saveButton.OnTapped = func() {
-		save := dialog.NewFileSave(func(file fyne.URIWriteCloser, err error) { // TODO: Might want to save this instead of recreating each time
-			if err != nil {
-				fyne.LogError("Error on selecting file to write to", err)
-				dialog.ShowError(err, d.window)
-				return
-			} else if file == nil {
-				return
-			}
-
-			if _, err := file.Write(received); err != nil {
-				fyne.LogError("Error on writing text to the file", err)
-				dialog.ShowError(err, d.window)
-			}
-
-			if err := file.Close(); err != nil {
-				fyne.LogError("Error on closing text file", err)
-				dialog.ShowError(err, d.window)
-			}
-		}, d.window)
-		now := time.Now().Format("2006-01-02T15:04") // TODO: Might want to use AppendFormat and strings.Builder
-		save.SetFileName("received-" + now + ".txt")
-		save.Resize(util.WindowSizeToDialog(d.window.Canvas().Size()))
-		save.Show()
-	}
-
-	d.textEntry.SetText(text)
-	d.window.Show()
-	d.window.RequestFocus()
-	d.window.Canvas().Focus(d.textEntry)
+	win := c.textRecvWindow.window
+	win.Show()
+	win.RequestFocus()
+	win.Canvas().Focus(c.textRecvWindow.textEntry)
 }
