4f934b8163719425e72c2a6aa5d4fb2b8bb53bbd
Jacob <jacalz@tutanota.com>
2024-01-11 15:29:22 +0100

    Don't recreate dialog for saving text file

--- internal/transport/display.go
+++ internal/transport/display.go
@@ -16,6 +16,7 @@ type textRecvWindow struct {
 	copyButton, saveButton *widget.Button
 	window                 fyne.Window
 	received               []byte
+	fileSaveDialog         *dialog.FileDialog
 }
 
 func (r *textRecvWindow) copy() {
@@ -27,30 +28,31 @@ func (r *textRecvWindow) interceptClose() {
 	r.textEntry.SetText("")
 }
 
+func (r *textRecvWindow) saveFileToDisk(file fyne.URIWriteCloser, err error) {
+	if err != nil {
+		fyne.LogError("Error on selecting file to write to", err)
+		dialog.ShowError(err, r.window)
+		return
+	} else if file == nil {
+		return
+	}
+
+	if _, err := file.Write(r.received); err != nil {
+		fyne.LogError("Error on writing text to the file", err)
+		dialog.ShowError(err, r.window)
+	}
+
+	if err := file.Close(); err != nil {
+		fyne.LogError("Error on closing text file", err)
+		dialog.ShowError(err, r.window)
+	}
+}
+
 func (r *textRecvWindow) save() {
-	save := dialog.NewFileSave(func(file fyne.URIWriteCloser, err error) { // TODO: Might want to save this instead of recreating each time
-		if err != nil {
-			fyne.LogError("Error on selecting file to write to", err)
-			dialog.ShowError(err, r.window)
-			return
-		} else if file == nil {
-			return
-		}
-
-		if _, err := file.Write(r.received); err != nil {
-			fyne.LogError("Error on writing text to the file", err)
-			dialog.ShowError(err, r.window)
-		}
-
-		if err := file.Close(); err != nil {
-			fyne.LogError("Error on closing text file", err)
-			dialog.ShowError(err, r.window)
-		}
-	}, r.window)
 	now := time.Now().Format("2006-01-02T15:04") // TODO: Might want to use AppendFormat and strings.Builder
-	save.SetFileName("received-" + now + ".txt")
-	save.Resize(util.WindowSizeToDialog(r.window.Canvas().Size()))
-	save.Show()
+	r.fileSaveDialog.SetFileName("received-" + now + ".txt")
+	r.fileSaveDialog.Resize(util.WindowSizeToDialog(r.window.Canvas().Size()))
+	r.fileSaveDialog.Show()
 }
 
 func (c *Client) createTextRecvWindow() {
@@ -58,10 +60,11 @@ func (c *Client) createTextRecvWindow() {
 	window.SetCloseIntercept(c.textRecvWindow.interceptClose)
 
 	c.textRecvWindow = textRecvWindow{
-		window:     window,
-		textEntry:  &widget.Entry{MultiLine: true, Wrapping: fyne.TextWrapWord},
-		copyButton: &widget.Button{Text: "Copy", Icon: theme.ContentCopyIcon(), OnTapped: c.textRecvWindow.copy},
-		saveButton: &widget.Button{Text: "Save", Icon: theme.DocumentSaveIcon(), OnTapped: c.textRecvWindow.save},
+		window:         window,
+		textEntry:      &widget.Entry{MultiLine: true, Wrapping: fyne.TextWrapWord},
+		copyButton:     &widget.Button{Text: "Copy", Icon: theme.ContentCopyIcon(), OnTapped: c.textRecvWindow.copy},
+		saveButton:     &widget.Button{Text: "Save", Icon: theme.DocumentSaveIcon(), OnTapped: c.textRecvWindow.save},
+		fileSaveDialog: dialog.NewFileSave(c.textRecvWindow.saveFileToDisk, window),
 	}
 
 	actionContainer := container.NewGridWithColumns(2, c.textRecvWindow.copyButton, c.textRecvWindow.saveButton)
