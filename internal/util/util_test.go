0ba601e7074061ad68b43137de7942f3b8bced04
Jacob <jacalz@tutanota.com>
2024-01-15 19:47:22 +0100

    Optimize code validator to not use regexp

--- internal/util/util_test.go
+++ internal/util/util_test.go
@@ -7,6 +7,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var globalValidationError error
+
+func BenchmarkCodeValidator(b *testing.B) {
+	local := error(nil)
+
+	for i := 0; i < b.N; i++ {
+		local = CodeValidator("125-upset-universe-mistake")
+	}
+
+	globalValidationError = local
+}
+
 func TestCodeValidator(t *testing.T) {
 	validate := CodeValidator
 
@@ -15,6 +27,21 @@ func TestCodeValidator(t *testing.T) {
 	assert.NoError(t, valid)
 
 	valid = validate("invalid-code")
+	assert.Error(t, valid)
+
+	valid = validate("IOI-")
+	assert.Error(t, valid)
+
+	valid = validate("126-")
+	assert.Error(t, valid)
+
+	valid = validate("126--almost--valid")
+	assert.Error(t, valid)
+
+	valid = validate("126-almost--valid")
+	assert.Error(t, valid)
+
+	valid = validate("126-almost-valid--")
 	assert.Error(t, valid)
 
 	valid = validate("15")
