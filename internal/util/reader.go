c459e13bb8ae02b0393936d6850dea588e596a71
Jacob <jacalz@tutanota.com>
2024-01-31 18:36:16 +0100

    Don't let the readers escape to the heap

--- internal/util/reader.go
+++ internal/util/reader.go
@@ -11,15 +11,15 @@ type ProgressReader struct {
 }
 
 // Read wraps the Reader and updates the progress bar.
-func (p *ProgressReader) Read(buf []byte) (int, error) {
+func (p ProgressReader) Read(buf []byte) (int, error) {
 	n, err := p.read.Read(buf)
 	p.progress(int64(n), p.Max)
 	return n, err
 }
 
 // NewProgressReader returns a wrapped Reader that updates the progress bar.
-func NewProgressReader(r io.Reader, p func(delta int64, max int64), max int64) *ProgressReader {
-	return &ProgressReader{read: r, progress: p, Max: max}
+func NewProgressReader(r io.Reader, p func(delta int64, max int64), max int64) ProgressReader {
+	return ProgressReader{read: r, progress: p, Max: max}
 }
 
 // ProgressReaderAt is a reader wrapper that calls a function for each part content being read with ReatAt.
@@ -31,13 +31,13 @@ type ProgressReaderAt struct {
 }
 
 // ReadAt wraps the ReaderAt and updates the progress bar.
-func (p *ProgressReaderAt) ReadAt(buf []byte, off int64) (int, error) {
+func (p ProgressReaderAt) ReadAt(buf []byte, off int64) (int, error) {
 	n, err := p.read.ReadAt(buf, off)
 	p.progress(int64(n), p.Max)
 	return n, err
 }
 
 // NewProgressReaderAt returns a wrapped ReaderAt that updates the progress bar.
-func NewProgressReaderAt(r io.ReaderAt, p func(delta int64, max int64), max int64) *ProgressReaderAt {
-	return &ProgressReaderAt{read: r, progress: p, Max: max}
+func NewProgressReaderAt(r io.ReaderAt, p func(delta int64, max int64), max int64) ProgressReaderAt {
+	return ProgressReaderAt{read: r, progress: p, Max: max}
 }
