8443b3f7817bb1be06f2e8f4dab718da3d5308c3
Jacalz <jacalz@tutanota.com>
2025-01-04 12:02:57 +0100

    Mockup send tab

--- internal/ui/send.go
+++ internal/ui/send.go
@@ -2,9 +2,25 @@ package ui
 
 import (
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
 func createSendPage(_ fyne.App, _ fyne.Window) fyne.CanvasObject {
-	return widget.NewLabel("Send something")
+	icon := canvas.NewImageFromResource(theme.UploadIcon())
+	icon.SetMinSize(fyne.NewSquareSize(200))
+
+	title := &widget.Label{Text: "Send Data", Alignment: fyne.TextAlignCenter, TextStyle: fyne.TextStyle{Bold: true}}
+	description := &widget.Label{Text: "Select data type below or drop files here", Alignment: fyne.TextAlignCenter}
+
+	// Buttons for starting sends.
+	file := &widget.Button{Icon: theme.FileTextIcon(), Text: "Send File"}
+	folder := &widget.Button{Icon: theme.FolderIcon(), Text: "Send Folder"}
+	text := &widget.Button{Icon: theme.DocumentIcon(), Text: "Send Text"}
+
+	buttons := container.NewVBox(icon, &widget.Separator{}, title, description, &widget.Separator{}, file, folder, text)
+
+	return container.NewCenter(buttons)
 }
