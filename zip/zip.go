b28e9db836622cc59c96eb533f169b3e72837fb5
Jacalz <jacalz@tutanota.com>
2025-07-08 11:42:12 +0200

    zip: Do not depend on fyne

--- zip/zip.go
+++ zip/zip.go
@@ -8,7 +8,6 @@ import (
 	"path/filepath"
 	"strings"
 
-	"fyne.io/fyne/v2"
 	"github.com/klauspost/compress/zip"
 )
 
@@ -28,7 +27,6 @@ var (
 func ExtractSafe(source io.ReaderAt, length int64, target string, uncompressedBytes int64, files int) error {
 	reader, err := zip.NewReader(source, length)
 	if err != nil {
-		fyne.LogError("Could not create zip reader", err)
 		return err
 	}
 
@@ -60,7 +58,6 @@ func ExtractSafe(source io.ReaderAt, length int64, target string, uncompressedBy
 func Extract(source io.ReaderAt, length int64, target string) error {
 	reader, err := zip.NewReader(source, length)
 	if err != nil {
-		fyne.LogError("Could not create zip reader", err)
 		return err
 	}
 
@@ -76,31 +73,26 @@ func Extract(source io.ReaderAt, length int64, target string) error {
 func extractFile(file *zip.File, target string) (err error) {
 	path, err := filepath.Abs(filepath.Join(target, file.Name))
 	if err != nil {
-		fyne.LogError("Could not calculate the ABS path", err)
 		return err
 	}
 
 	if !strings.HasPrefix(path, target) {
-		fyne.LogError("Dangerous filename detected", ErrorDangerousFilename)
 		return ErrorDangerousFilename
 	}
 
 	fileReader, err := file.Open()
 	if err != nil {
-		fyne.LogError("Could not open the zip file", err)
 		return err
 	}
 
 	defer func() {
 		if cerr := fileReader.Close(); cerr != nil {
-			fyne.LogError("Could not close the zip file reader", err)
 			err = cerr
 		}
 	}()
 
 	if file.FileInfo().IsDir() {
 		if err := os.MkdirAll(path, 0o750); err != nil {
-			fyne.LogError("Could not create the directory", err)
 			return err
 		}
 
@@ -108,26 +100,22 @@ func extractFile(file *zip.File, target string) (err error) {
 	}
 
 	if err := os.MkdirAll(filepath.Dir(path), 0o750); err != nil {
-		fyne.LogError("Could not create the directory", err)
 		return err
 	}
 
 	targetFile, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, file.Mode()) // #nosec - The path has already been cleaned by filepath.Abs()
 	if err != nil {
-		fyne.LogError("Could not create the target file", err)
 		return err
 	}
 
 	defer func() {
 		if cerr := targetFile.Close(); cerr != nil {
-			fyne.LogError("Could not close the target file", err)
 			err = cerr
 		}
 	}()
 
 	_, err = io.Copy(targetFile, fileReader)
 	if err != nil {
-		fyne.LogError("Could not copy the contents", err)
 		return err
 	}
 
